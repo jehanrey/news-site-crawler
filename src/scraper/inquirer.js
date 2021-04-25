655822c307bc8b36752c8dbb627b22899dc5db40
Jehan Reyes <jehan.reyes@whitecloak.com>
2021-04-25 11:55:38 +0800

    stage

--- src/scraper/inquirer.js
+++ src/scraper/inquirer.js
@@ -11,27 +11,70 @@ async function inquirerScraper ({
     let dataObj = {}
     let newPage = await browser.newPage()
     console.log(`navigating to ${link}...`)
-    await newPage.goto(link, {waitUntil: 'domcontentloaded', timeout: 0})
-    // await newPage.waitForSelector('#article_level_wrap').catch(() => skip = true)
+    await newPage.goto(link, {waitUntil: 'load', timeout: 0})
 
     dataObj["url"] = link
-    dataObj["title"] = await newPage.$eval("#art-head-group hgroup > h1.entry-title", (text) => text.textContent).catch(() => skip = true)
-    dataObj["date"] = await newPage.$eval("#art-head-group #byline_share div#art_plat", (text) => (
-      text.textContent.split('/')[1].trim()
-    )).catch(() => skip = true)
-    dataObj["snippet"] = await newPage.$eval("#art_body_wrap > #article_content > div:first-child > p:first-of-type", (text) => {
-      return text.textContent.replace(/(\s\r\n\t|\n|\r|\t|&nbsp;|<br>|<br \/>)/gm, "").trim()
-    }).catch(() => '')
-    dataObj["body"] = await newPage.$eval("#art_body_wrap > #article_content > div:first-child", (el) => {
-      const childNodes = el.children
-      let contents = []
-      for (let item of childNodes) {
-        if (item.nodeName === 'P') {
-          contents.push(item.textContent.replace(/(\s\r\n\t|\n|\r|\t|&nbsp;|<br>|<br \/>)/gm, "").trim())
+    if (link.includes('libre')) {
+      dataObj["title"] = await newPage.$eval("#article-wrap > #libre-head > h1", (text) => text.textContent).catch(() => skip = true)
+      dataObj["date"] = await newPage.$eval("#article-wrap > #libre-head > h2 > span:last-child", (text) => text.textContent.trim()).catch(() => skip = true)
+      dataObj["snippet"] = ''
+      dataObj["body"] = await newPage.$eval("#article-wrap > #libre-head + #article-content", (el) => {
+        const childNodes = el.children
+        let contents = []
+        for (let item of childNodes) {
+          if (item.nodeName === 'P') {
+            contents.push(item.textContent.replace(/(\s\r\n\t|\n|\r|\t|&nbsp;|<br>|<br \/>)/gm, "").trim())
+          }
         }
-      }
-      return contents.join(' ')
-    }).catch(() => skip = true)
+        return contents.join(' ')
+      }).catch(() => skip = true)
+    } else if (link.includes('preen')) {
+      dataObj["title"] = await newPage.$eval("main#main > article > header.page-header > h1", (text) => text.textContent).catch(() => skip = true)
+      dataObj["date"] = await newPage.$eval("main#main > article > header.page-header > .post-meta > .meta-date", (text) => text.textContent.trim()).catch(() => skip = true)
+      dataObj["snippet"] = ''
+      dataObj["body"] = await newPage.$eval("main#main > article > .post-wrap section.entry-content", (el) => {
+        const childNodes = el.children
+        let contents = []
+        for (let item of childNodes) {
+          if (item.nodeName === 'P') {
+            contents.push(item.textContent.replace(/(\s\r\n\t|\n|\r|\t|&nbsp;|<br>|<br \/>)/gm, "").trim())
+          }
+        }
+        return contents.join(' ')
+      }).catch(() => skip = true)
+    } else if (link.includes('motioncars') || link.includes('cebudailynews')) {
+      dataObj["title"] = await newPage.$eval("#landing-main-article > #landing-headline > h1", (text) => text.textContent).catch(() => skip = true)
+      dataObj["date"] = await newPage.$eval("#landing-main-article > #landing-headline > h1 + div > span:last-child", (text) => text.textContent.trim()).catch(() => skip = true)
+      dataObj["snippet"] = ''
+      dataObj["body"] = await newPage.$eval("#landing-main-article > #article-content", (el) => {
+        const childNodes = el.children
+        let contents = []
+        for (let item of childNodes) {
+          if (item.nodeName === 'P') {
+            contents.push(item.textContent.replace(/(\s\r\n\t|\n|\r|\t|&nbsp;|<br>|<br \/>)/gm, "").trim())
+          }
+        }
+        return contents.join(' ')
+      }).catch(() => skip = true)
+    } else {
+      dataObj["title"] = await newPage.$eval("#art-head-group hgroup > h1.entry-title", (text) => text.textContent).catch(() => skip = true)
+      dataObj["date"] = await newPage.$eval("#art-head-group #byline_share div#art_plat", (text) => (
+        text.textContent.split('/')[1].trim()
+      )).catch(() => skip = true)
+      dataObj["snippet"] = await newPage.$eval("#art_body_wrap > #article_content > div:first-child > p:first-of-type", (text) => {
+        return text.textContent.replace(/(\s\r\n\t|\n|\r|\t|&nbsp;|<br>|<br \/>)/gm, "").trim()
+      }).catch(() => '')
+      dataObj["body"] = await newPage.$eval("#art_body_wrap > #article_content > div:first-child", (el) => {
+        const childNodes = el.children
+        let contents = []
+        for (let item of childNodes) {
+          if (item.nodeName === 'P') {
+            contents.push(item.textContent.replace(/(\s\r\n\t|\n|\r|\t|&nbsp;|<br>|<br \/>)/gm, "").trim())
+          }
+        }
+        return contents.join(' ')
+      }).catch(() => skip = true)
+    }
 
     resolve(skip ? undefined : dataObj)
     counter = skip ? counter : counter + 1
